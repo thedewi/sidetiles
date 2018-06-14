56b0c5917071746ec2e6ece26509825c44f1ced6
thedewi <thedewi@users.noreply.github.com>
2018-06-14 17:01:22 +1000

    implement double-click appbar toggle

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppBar;
 
 namespace SideTiles
 {
@@ -20,9 +22,25 @@ namespace SideTiles
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _barred;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+
+            AppBarFunctions.SetAppBar(this, _barred ? ABEdge.None : ABEdge.Right);
+            _barred = !_barred;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            AppBarFunctions.SetAppBar(this, ABEdge.None);
         }
     }
 }
